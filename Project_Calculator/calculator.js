2238fa4358b6aee6f3de5c0ebec7018fa378d86e
Hamit <hamitsehjal26@gmail.com>
2022-11-14 07:19:03 -0500

    designed functions"handleNumbers", "handleSymbols"

--- Project_Calculator/calculator.js
+++ Project_Calculator/calculator.js
@@ -1,8 +1,33 @@
 
+function buttonClick(value) {
+    console.log(value);
+
+    if (isNaN(parseInt(value))) {
+        // if the value is not a number --> which means it is a symbol!!
+        handleSymbols(value);
+    }
+    else {
+        handleNumbers(value);
+    }
+}
+
+function handleNumbers(value) {
+    console.log(value+" Number!!");
+
+}
+
+function handleSymbols(value) {
+    console.log(value+" Symbol!!");
+
+}
 
 function initialize() {
-    document.querySelector("grid-container")
+
+    console.log("INITIALIZED !!");
+    document.querySelector(".grid-container")
         .addEventListener("click", function (event) {
             buttonClick(event.target.innerText);
         })
 }
+
+initialize();
