011f497547ce68e2f2d7dbffd171d822f664ec02
Hamit <hamitsehjal26@gmail.com>
2022-11-19 14:34:08 -0500

    designed logic to write on each row of the grid!!

--- Project_Wordle/wordle.js
+++ Project_Wordle/wordle.js
@@ -6,21 +6,37 @@ const ANSWER_LENGTH = 5
 async function init() {
 
     let currentGuess = ''
+    let currentRow = 0
 
     function isLetter(letter) {
         return /^[a-zA-Z]$/.test(letter);
     }
 
     function addLetter(letter) {
+
+        // add the letter to the end !!
         if (currentGuess.length < ANSWER_LENGTH) {
             currentGuess += letter;
         }
+        // replace the last letter!!
         else {
             currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
         }
 
         // This line will show the letters on the Screen!!
-        letters[currentGuess.length - 1].innerText = letter;
+        // "ANSWER_LENGHT * currentRow" in the below expression will make sure that each time
+        // we press enter, we get to write on the next row!!
+        letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText = letter;
+    }
+
+    async function commit() {
+        if (currentGuess.length !== ANSWER_LENGTH) {
+            // do nothing!!
+            return;
+        }
+
+        currentGuess = ''
+        currentRow++;
     }
     document.addEventListener('keydown', function handleKeyPress(event) {
         const action = event.key;
