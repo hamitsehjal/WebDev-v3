e6a110293bacd68edec794b65cf01a49b250244a
Hamit <hamitsehjal26@gmail.com>
2022-11-19 13:41:40 -0500

    added some skeleton code!!

--- Project_Wordle/wordle.js
+++ Project_Wordle/wordle.js
@@ -3,8 +3,8 @@ const letters = document.querySelectorAll(".flex-item")
 console.log(letters.length)
 
 
-const loadingIndicator = document.querySelector(".loading");
-console.log(loadingIndicator.textContent)
+// const loadingIndicator = document.querySelector(".loading");
+// console.log(loadingIndicator.textContent)
 
 
 
@@ -12,6 +12,22 @@ function init() {
     document.addEventListener('keydown', function handleKeyPress(event) {
         const action = event.key;
         console.log(action)
+
+        if(action==='Enter'){
+            commit(); // which will save the word to compare it to the result!!
+        }
+        else if(action==='Backspace')
+        {
+           backspace();
+        }
+        else if(isletter(action))
+        {
+            addLetter(action.toUpperCase());
+        }
+        else{
+            // Do Nothing !!
+        }
+
     })
 }
 
