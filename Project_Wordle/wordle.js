381bb1f1adbe49c078085f38915568f40a201bad
Hamit <hamitsehjal26@gmail.com>
2022-11-19 13:53:03 -0500

    added function "addLetter" and "isLetter" !!

--- Project_Wordle/wordle.js
+++ Project_Wordle/wordle.js
@@ -1,30 +1,37 @@
 const letters = document.querySelectorAll(".flex-item")
-
-console.log(letters.length)
-
-
-// const loadingIndicator = document.querySelector(".loading");
-// console.log(loadingIndicator.textContent)
+const ANSWER_LENGTH = 5
 
 
 
-function init() {
+async function init() {
+
+    const currentGuess = ''
+    function isLetter(letter) {
+        return /^[a-zA-Z]$/.test(letter);
+    }
+
+    function addLetter(letter) {
+        if (currentGuess.length < ANSWER_LENGTH) {
+            currentGuess += letter;
+        }
+        else {
+            currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter;
+        }
+    }
     document.addEventListener('keydown', function handleKeyPress(event) {
         const action = event.key;
         console.log(action)
 
-        if(action==='Enter'){
+        if (action === 'Enter') {
             commit(); // which will save the word to compare it to the result!!
         }
-        else if(action==='Backspace')
-        {
-           backspace();
+        else if (action === 'Backspace') {
+            backspace();
         }
-        else if(isletter(action))
-        {
+        else if (isLetter(action)) {
             addLetter(action.toUpperCase());
         }
-        else{
+        else {
             // Do Nothing !!
         }
 
